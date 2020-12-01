9a4ee47c42711b037382ff5af883a2987533caf9
refawz <73493893+refawz@users.noreply.github.com>
2020-12-01 19:21:17 +0700

    #1 job with chuck

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    // -------------- On Scroll Navbar Effect -------------- 
+    var window_width = $(window).width();
+
+    $(window).scroll(function(){  
+      "use strict"; 
+      var scroll = $(window).scrollTop();
+      if( scroll > 60 ){
+        $(".navbar").addClass("scroll-fixed-navbar");
+      } else {
+        $(".navbar").removeClass("scroll-fixed-navbar");
+      }
+    });
+
+    $('.faq-item .link').click(function() {
+      $(this).closest('.faq-item').toggleClass('no-lineclamp');
+      $(this).toggleClass('show-less');
+    });
+
+    // ----------------------- Slick ------------------------
+
+    $('.head-nav').slick({
+      variableWidth: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      dots: false,
+      arrows: true,
+      prevArrow:'<a class="slick-prev"><span class="fa fa-caret-left"></span></a>',
+      nextArrow:'<a class="slick-next"><span class="fa fa-caret-right"></span></a>'
+    });
+
+    // --------------------- Datepicker -----------------------
+
+    $( "#datepicker" ).datepicker();
+
+
+    // --------------------- Timepicker -----------------------
+
+    $('input.timepicker').timepicker({});
+});
